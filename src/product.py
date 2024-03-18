650f5413eea6d1c0f8d353388268f8983c517b06
ikachaeva <irenkachaeva@gmail.com>
2024-03-19 02:25:30 +0300

    13.2 3,4 задание из домашней работы

--- src/product.py
+++ src/product.py
@@ -9,5 +9,24 @@ class Product:
     def __init__(self, name, description, price, quantity_stock):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity_stock = quantity_stock
+
+    @classmethod
+    def new_product(cls, name: str, description: str, price: float, quantity_stock: int):
+        '''создание нового товара'''
+        new_product = cls(name, description, price, quantity_stock)
+        return new_product
+
+    '''геттер для атрибута цены.'''
+    @property
+    def price(self):
+        return self.__price
+
+    '''cеттер для атрибута цены. В случае если цена равна или ниже нуля, выводится сообщение в консоль, при этом новая цена не устанавливается.'''
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Введена некорректная цена")
+        else:
+            self.__price = new_price #устанавливаем цену, если она удовлетворяет условию выше
--- tests/test_product.py
+++ tests/test_product.py
@@ -12,5 +12,14 @@ def test_init_product(product_f):
     assert product_f.price == 180000.0
     assert product_f.quantity_stock == 5
 
+#def test_new_product():
+   # assert Product.new_product("Телефон", "мобильный", 10000, 5) == Product("Телефон", "мобильный", 10000, 5)
 
+@pytest.fixture
+def product_3():
+    return Product("iPhone", "smth", 1000, 32)
+
+def test_get_price(product_3):
+    product_3.price = 1000
+    assert print(product_3.price) == 1000
 
