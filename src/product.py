f88d6c18d70298a9e4002c5ab108cf38f640f4c2
ira_kachaeva <irenkachaeva@gmail.com>
2024-03-31 14:58:52 +0300

    15.1 1, 2 задания

--- src/product.py
+++ src/product.py
@@ -38,5 +38,27 @@ class Product:
         '''складываем объекты между собой таким образом,
         чтобы результат выполнения сложения двух продуктов был сложением сумм,
         умноженных на количество на складе.'''
-        return self.quantity_stock * self.price + other.quantity_stock * other.price
+        if type(self) == type(other):
+
+            return self.quantity_stock * self.price + other.quantity_stock * other.price
+        raise TypeError('Можно складывать товары только из одинаковых классов продуктов')
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity_stock, productivity, model, memory, color):
+
+        super().__init__(name, description, price, quantity_stock)
+        self.productivity = productivity #производительность
+        self.model = model #модель
+        self.memory = memory #объем встроенной памяти
+        self.color = color #цвет.
+
+
+class LawnGrass(Product):
+
+    def __init__(self, name, description, price, quantity_stock, country, germination_period, color):
+        super().__init__(name, description, price, quantity_stock)
+        self.country = country #страна - производитель
+        self.germination_period = germination_period #срок прорастания,
+        self.color = color #цвет.
 
